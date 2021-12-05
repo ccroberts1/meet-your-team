91995f17195f2b1b815e661534701e35d6884c69
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2021-12-04 18:06:09 -0700

    extended employee class with intern subclass, added testing for that subclass

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,21 @@
+const Intern = require("../lib/intern");
+
+describe("Intern", () => {
+  test("should set school via constructor", () => {
+    const obj = new Intern("john doe", 3, "johndoe@email.com", "DU");
+    expect(obj.school).toEqual("DU");
+  });
+  test("should return the Manager object", () => {
+    const obj = new Intern("Sam", 2, "sam@email.com", "OSU");
+    const returnIntern = () => obj.getRole();
+
+    expect(returnIntern()).toEqual(obj);
+  });
+
+  test("should retrieve the school property", () => {
+    const obj = new Intern("Sam", 2, "sam@email.com", "OSU");
+    const newSchool = () => obj.getSchool();
+
+    expect(newSchool()).toBe("OSU");
+  });
+});
